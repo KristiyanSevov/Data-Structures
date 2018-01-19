ddaec59f75c90c528089290370d925abda55fa93
KristiyanSevov <kristiyan.sevov@gmail.com>
2018-01-19 14:33:00 +0200

    Added exercise

---
+++ 05. Trees and Binary Trees (Lab)/Trees/BinaryTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    private T value;
+    private BinaryTree<T> leftChild;
+    private BinaryTree<T> rightChild;
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.value = value;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        BinaryTree<T> root = this;
+        this.PrintPreOrder(root, indent);
+    }
+
+    private void PrintPreOrder(BinaryTree<T> node, int indent)
+    {
+        if (node != null)
+        {
+            Console.WriteLine(new string(' ', indent) + node.value);
+            PrintPreOrder(node.leftChild, indent + 2);
+            PrintPreOrder(node.rightChild, indent + 2);
+        }   
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        BinaryTree<T> root = this;
+        this.EachInOrder(action, root);
+    }
+
+    private void EachInOrder(Action<T> action, BinaryTree<T> node)
+    {
+        if (node != null)
+        {
+            EachInOrder(action, node.leftChild);
+            action(node.value);
+            EachInOrder(action, node.rightChild);
+        }
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        BinaryTree<T> root = this;
+        this.EachPostOrder(action, root);
+    }
+
+    private void EachPostOrder(Action<T> action, BinaryTree<T> node)
+    {
+        if (node != null)
+        {
+            EachPostOrder(action, node.leftChild);
+            EachPostOrder(action, node.rightChild);
+            action(node.value);
+        }
+    }
+}
---
+++ 05. Trees and Binary Trees (Lab)/Trees/Tree.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    private T value;
+    private List<Tree<T>> children;
+  
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.value = value;
+        this.children = new List<Tree<T>>(children);
+    }
+
+    public void Print(int indent = 0)
+    {
+        Tree<T> root = this;
+        this.PrintTree(root, indent);
+    }
+
+    private void PrintTree(Tree<T> node, int indent)
+    {
+        Console.WriteLine(new string(' ', indent) + node.value);
+        foreach (var child in node.children)
+        {
+            PrintTree(child, indent + 2);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(this.value);
+        foreach (var child in this.children)
+        {
+            child.Each(action);
+        }
+    }
+
+    ////Elegant but not very efficient
+    //public IEnumerable<T> OrderDFS()
+    //{
+    //    foreach (var child in this.children)
+    //    {
+    //        foreach (T value in child.OrderDFS())
+    //        {
+    //            yield return value;
+    //        }
+    //    }
+    //    yield return this.value;
+    //}
+
+    public IEnumerable<T> OrderDFS()
+    {
+        List<T> result = new List<T>();
+        Tree<T> root = this;
+        this.OrderDFS(root, result);
+        return result;
+    }
+
+    private void OrderDFS(Tree<T> node, List<T> result)
+    {
+        foreach (var child in node.children)
+        {
+            OrderDFS(child, result);
+        }
+        result.Add(node.value);
+    }
+
+
+    public IEnumerable<T> OrderBFS()
+    {
+        Queue<Tree<T>> queue = new Queue<Tree<T>>();
+        queue.Enqueue(this);
+        while (queue.Count > 0)
+        {
+            Tree<T> node = queue.Dequeue();
+            yield return node.value;
+            foreach (var child in node.children)
+            {
+                queue.Enqueue(child);
+            }
+        }
+    }
+}
