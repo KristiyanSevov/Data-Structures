1ad13e21a373b56286e7cea1420fb39904a814c7
KristiyanSevov <kristiyan.sevov@gmail.com>
2018-01-31 18:53:00 +0200

    Added exercises

---
+++ 09. Heaps and HeapSort (Lab)/BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinaryHeap<T> where T : IComparable<T>
+{
+    private List<T> heap;
+
+    public BinaryHeap()
+    {
+        this.heap = new List<T>();
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.heap.Count;
+        }
+    }
+
+    public void Insert(T item)
+    {
+        this.heap.Add(item);
+        this.MaxHeapifyUp(this.heap.Count - 1);
+    }
+
+    private void MaxHeapifyUp(int index)
+    {
+        while (index != 0)
+        {
+            int parentIndex = (index - 1) / 2;
+            if (this.heap[index].CompareTo(this.heap[parentIndex]) < 0)
+            {
+                break;
+            }
+            Swap(index, parentIndex);
+            index = parentIndex;
+        }
+    }
+
+    private void Swap(int index, int parentIndex)
+    {
+        T temp = this.heap[parentIndex];
+        this.heap[parentIndex] = this.heap[index];
+        this.heap[index] = temp;
+    }
+
+    public T Peek()
+    {
+        if (this.heap.Count == 0)
+        {
+            throw new InvalidOperationException("heap is empty"); 
+        }
+        return this.heap[0];
+    }
+
+    public T Pull()
+    {
+        if (this.heap.Count == 0)
+        {
+            throw new InvalidOperationException("heap is empty");
+        }
+        T result = this.heap[0];
+        this.Swap(0, this.heap.Count - 1);
+        this.heap.RemoveAt(this.heap.Count - 1);
+        this.MaxHeapifyDown(0);
+        return result;
+    }
+
+    private void MaxHeapifyDown(int index)
+    {
+        while (index <= this.heap.Count / 2)
+        {
+            int leftIndex = index * 2 + 1;
+            int rightIndex = index * 2 + 2;
+            int childIndex;
+            if (leftIndex >= this.Count)
+            {
+                break;
+            }
+            else if (rightIndex >= this.Count)
+            {
+                childIndex = leftIndex;
+            }
+            else
+            {
+                childIndex = this.heap[leftIndex].CompareTo(this.heap[rightIndex]) > 0 ? leftIndex : rightIndex;
+            }
+            if (this.heap[index].CompareTo(this.heap[childIndex]) > 0)
+            {
+                break;
+            }
+            Swap(index, childIndex);
+            index = childIndex;
+        }
+    }
+}
---
+++ 09. Heaps and HeapSort (Lab)/BinaryHeap/Heap.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public static class Heap<T> where T : IComparable<T>
+{
+    public static void Sort(T[] arr)
+    {
+        for (int i = arr.Length / 2; i >= 0; i--)
+        {
+            int leftIndex = i * 2 + 1;
+            int rightIndex = i * 2 + 2;
+            int childIndex;
+            if (leftIndex >= arr.Length)
+            {
+                continue;
+            }
+            else if (rightIndex >= arr.Length)
+            {
+                childIndex = leftIndex;
+            }
+            else
+            {
+                childIndex = arr[leftIndex].CompareTo(arr[rightIndex]) > 0 ? leftIndex : rightIndex;
+            }
+            if (arr[i].CompareTo(arr[childIndex]) < 0)
+            {
+                Swap(arr, i, childIndex);
+            }
+        }
+
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
+            Swap(arr, 0, i);
+            MaxHeapifyDown(arr, 0, i - 1);
+        }
+    }
+
+    private static void MaxHeapifyDown(T[] arr, int index, int endIndex)
+    {
+        while (index <= endIndex / 2)
+        {
+            int leftIndex = index * 2 + 1;
+            int rightIndex = index * 2 + 2;
+            int childIndex;
+            if (leftIndex > endIndex)
+            {
+                break;
+            }
+            else if (rightIndex > endIndex)
+            {
+                childIndex = leftIndex;
+            }
+            else
+            {
+                childIndex = arr[leftIndex].CompareTo(arr[rightIndex]) > 0 ? leftIndex : rightIndex;
+            }
+            if (arr[index].CompareTo(arr[childIndex]) > 0)
+            {
+                break;
+            }
+            Swap(arr, index, childIndex);
+            index = childIndex;
+        }
+    }
+
+    private static void Swap(T[] arr, int index, int parentIndex)
+    {
+        T temp = arr[parentIndex];
+        arr[parentIndex] = arr[index];
+        arr[index] = temp;
+    }
+}
