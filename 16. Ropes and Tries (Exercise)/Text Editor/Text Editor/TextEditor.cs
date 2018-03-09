e29cb878893470421e4f02388c51c4f652fed984
KristiyanSevov <kristiyan.sevov@gmail.com>
2018-03-09 20:42:02 +0200

    Added exercises

---
+++ 16. Ropes and Tries (Exercise)/Text Editor/Text Editor/TextEditor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace Text_Editor
+{
+    class TextEditor : ITextEditor
+    {
+        private HashSet<string> loggedUsers = new HashSet<string>();
+        private Trie<BigList<char>> users = new Trie<BigList<char>>();
+        private Dictionary<string, Stack<BigList<char>>> cache = 
+            new Dictionary<string, Stack<BigList<char>>>();
+       
+        public void Clear(string username)
+        {
+            BigList<char> str = users.GetValue(username);
+            cache[username].Push(new BigList<char>(str));
+            str.Clear();
+        }
+
+        public void Delete(string username, int startIndex, int length)
+        {
+            BigList<char> str = users.GetValue(username);
+            cache[username].Push(new BigList<char>(str));
+            str.RemoveRange(startIndex, length);
+        }
+
+        public void Insert(string username, int index, string str)
+        {
+            BigList<char> user_str = users.GetValue(username);
+            cache[username].Push(new BigList<char>(user_str));
+            user_str.InsertRange(index, str);
+        }
+
+        public int Length(string username)
+        {
+            return String.Join("", users.GetValue(username)).Length;
+        }
+
+        public void Login(string username)
+        {
+            if (loggedUsers.Contains(username))
+            {
+                this.Clear(username);
+            }
+            else
+            {
+                loggedUsers.Add(username);
+                cache.Add(username, new Stack<BigList<char>>());
+                users.Insert(username, new BigList<char>());
+            }
+        }
+
+        public void Logout(string username)
+        {
+            loggedUsers.Remove(username);
+            cache.Remove(username);
+        }
+
+        public void Prepend(string username, string str)
+        {
+            BigList<char> user_str = users.GetValue(username);
+            cache[username].Push(new BigList<char>(user_str));
+            user_str.InsertRange(0, str);
+        }
+
+        public string Print(string username)
+        {
+            return String.Join("", users.GetValue(username));
+        }
+
+        public void Substring(string username, int startIndex, int length)
+        {
+            BigList<char> user_str = users.GetValue(username);
+            cache[username].Push(new BigList<char>(user_str));
+            BigList<char> newList = new BigList<char>(user_str.Range(startIndex, length));
+            users.Insert(username, newList);
+        }
+
+        public void Undo(string username)
+        {
+            BigList<char> newList = cache[username].Pop();
+            users.Insert(username, newList);
+        }
+
+        public IEnumerable<string> Users(string prefix = "")
+        {
+            return users.GetByPrefix(prefix).Where(x => loggedUsers.Contains(x));
+        }
+
+        public bool IsLogged(string username)
+        {
+            return loggedUsers.Contains(username);
+        }
+    }
+}
---
+++ 21. Combining Data Structures (Lab)/PersonCollection/PersonCollection.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class PersonCollection : IPersonCollection
+{
+    private Dictionary<string, Person> peopleByEmail = new Dictionary<string, Person>();
+    private Dictionary<string, SortedSet<Person>> peopleByDomain = new Dictionary<string, SortedSet<Person>>();
+    private Dictionary<string, SortedSet<Person>> peopleByNameAndTown 
+        = new Dictionary<string, SortedSet<Person>>();
+    private OrderedMultiDictionary<int, Person> peopleByAge = new OrderedMultiDictionary<int, Person>(true);
+    private Dictionary<string, OrderedMultiDictionary<int, Person>> peopleByTownAndAge =
+        new Dictionary<string, OrderedMultiDictionary<int, Person>>();
+
+    public bool AddPerson(string email, string name, int age, string town)
+    {
+        if (peopleByEmail.ContainsKey(email))
+        {
+            return false;
+        }
+        Person person = new Person(email, name, age, town);
+        peopleByEmail.Add(email, person);
+        string domain = email.Substring(email.IndexOf('@')+1);
+        if (!peopleByDomain.ContainsKey(domain))
+        {
+            peopleByDomain.Add(domain, new SortedSet<Person>());
+        }
+        peopleByDomain[domain].Add(person);
+        string nameAndTown = name + town;
+        if (!peopleByNameAndTown.ContainsKey(nameAndTown))
+        {
+            peopleByNameAndTown.Add(nameAndTown, new SortedSet<Person>());
+        }
+        peopleByNameAndTown[nameAndTown].Add(person);
+        peopleByAge.Add(age, person);
+        if (!peopleByTownAndAge.ContainsKey(town))
+        {
+            peopleByTownAndAge.Add(town, new OrderedMultiDictionary<int, Person>(true));
+        }
+        peopleByTownAndAge[town].Add(age, person);
+        return true;
+
+    }
+
+    public int Count
+    {
+        get
+        {
+            return peopleByEmail.Count;
+        }
+    }
+
+    public Person FindPerson(string email)
+    {
+        if (!peopleByEmail.ContainsKey(email))
+        {
+            return null;
+        }
+        return this.peopleByEmail[email];
+    }
+
+    public bool DeletePerson(string email)
+    {
+        if (!peopleByEmail.ContainsKey(email))
+        {
+            return false;
+        }
+        Person person = peopleByEmail[email];
+        peopleByEmail.Remove(email);
+        string domain = person.Email.Substring(person.Email.IndexOf('@') + 1);
+        peopleByDomain[domain].Remove(person);
+        peopleByNameAndTown[person.Name + person.Town].Remove(person);
+        peopleByAge[person.Age].Remove(person);
+        peopleByTownAndAge[person.Town][person.Age].Remove(person);
+        return true;
+    }
+
+    public IEnumerable<Person> FindPersons(string emailDomain)
+    {
+        if (peopleByDomain.ContainsKey(emailDomain))
+        {
+            return peopleByDomain[emailDomain];
+        }
+        return Enumerable.Empty<Person>();
+        
+    }
+
+    public IEnumerable<Person> FindPersons(string name, string town)
+    {
+        if (peopleByNameAndTown.ContainsKey(name+town))
+        {
+            return peopleByNameAndTown[name+town];
+        }
+        return Enumerable.Empty<Person>();
+    }
+
+    public IEnumerable<Person> FindPersons(int startAge, int endAge)
+    {
+        return peopleByAge.Range(startAge, true, endAge, true).Values;
+    }
+
+    public IEnumerable<Person> FindPersons(
+        int startAge, int endAge, string town)
+    {
+        if (peopleByTownAndAge.ContainsKey(town))
+        {
+            return peopleByTownAndAge[town].Range(startAge, true, endAge, true).Values;
+        }
+        return Enumerable.Empty<Person>();   
+    }
+}
